1ff69689713d01ccbb182bf7d06ebeed49fd5a7f
Exem <chernyakov.ghost@mail.ru>
2021-12-11 17:52:16 +0500

    ПОЧИНИТЬ НА 54 СТОРКЕ

--- lab8/Form1.cs
+++ lab8/Form1.cs
@@ -19,10 +19,57 @@ namespace lab8
             Ailer a = new Ailer(t0, T, u0, Convert.ToDouble(comboBox1.SelectedItem.ToString()));
             a.doAiler();
             List<double> AilerResults = a.uResults;
+
+            Ailer a1 = new Ailer(t0, T, u0, Convert.ToDouble(comboBox1.Items[1].ToString()));
+            a1.doAiler();
+            List<double> a1Results = a1.uResults;
+
+            Ailer a2 = new Ailer(t0, T, u0, Convert.ToDouble(comboBox1.SelectedItem.ToString()) / 3);
+            a2.doAiler();
+            List<double> a2Results = a2.uResults;
+
+            //string a1Res = "";
+            //for (int i = 0; i < a1Results.Count; i++)
+            //{
+            //    a1Res += a1Results[i].ToString() + "\n";
+            //}
+
+            //string aPath = $"shit({Convert.ToDouble(comboBox1.SelectedItem.ToString())}).txt";
+            //System.IO.StreamWriter SaveFileaa = new System.IO.StreamWriter(aPath);
+            //SaveFileaa.Write(a1Res);
+            //SaveFileaa.ToString();
+            //SaveFileaa.Close();
+
+            string a1diff = ""; string a2diff = "";
+            int o = 0;
+            for (int i = 0; i < AilerResults.Count; i++)
+            {
+                a1diff += (a1Results[i + o] - AilerResults[i]).ToString() + "\n";
+                o++;
+            }
+            o = 0;
+            Console.WriteLine(a2Results.Count);
+            for (int i = 0; i < a1Results.Count; i++)
+            {
+                a2diff += (a2Results[i + o] - a1Results[i]).ToString() + "\n"; //КАКОГО ХУЯ
+                o++;
+            }
+
+            string Path = $"difference1Ailer.txt";
+            System.IO.StreamWriter SaveFilea = new System.IO.StreamWriter(Path);
+            SaveFilea.Write(a1diff);
+            SaveFilea.ToString();
+            SaveFilea.Close();
+
+            Path = $"difference2Ailer.txt";
+            System.IO.StreamWriter SaveFileb = new System.IO.StreamWriter(Path);
+            SaveFileb.Write(a2diff);
+            SaveFileb.ToString();
+            SaveFileb.Close();
+
             foreach (double res in AilerResults) {
                 listBox1.Items.Add(res);
             }
-            //File.WriteAllText($"ailer{comboBox1.SelectedItem.ToString()}.txt", tempResults.ToString());
 
             string sPath = $"ailer{comboBox1.SelectedItem.ToString()}.txt";
 
@@ -39,6 +86,39 @@ namespace lab8
             List<double> predResults = pred.uResults;
             foreach (double res in predResults) listBox2.Items.Add(res);
 
+            predictor pred1 = new predictor(t0, T, u0, Convert.ToDouble(comboBox1.Items[1].ToString()));
+            pred1.doPredictor();
+            List<double> pred1Results = pred1.uResults;
+
+            predictor pred2 = new predictor(t0, T, u0, Convert.ToDouble(comboBox1.SelectedItem.ToString()) / 3);
+            pred2.doPredictor();
+            List<double> pred2Results = pred2.uResults;
+            string pred1diff = ""; string pred2diff = "";
+            o = 0;
+            for (int i = 0; i < predResults.Count; i++)
+            {
+                pred1diff += (pred1Results[i + o] - predResults[i]).ToString() + "\n";
+                o++;
+            }
+            o = 0;
+            for (int i = 0; i < pred1Results.Count; i++)
+            {
+                pred2diff += (pred2Results[i + o] - pred1Results[i]).ToString() + "\n";
+                o += 2;
+            }
+
+            Path = $"difference1Predictor.txt";
+            System.IO.StreamWriter SaveFilevv = new System.IO.StreamWriter(Path);
+            SaveFilevv.Write(pred1diff);
+            SaveFilevv.ToString();
+            SaveFilevv.Close();
+
+            Path = $"difference2Predictor.txt";
+            System.IO.StreamWriter SaveFileaaa = new System.IO.StreamWriter(Path);
+            SaveFileaaa.Write(pred2diff);
+            SaveFileaaa.ToString();
+            SaveFileaaa.Close();
+
             sPath = $"predictor{comboBox1.SelectedItem.ToString()}.txt";
 
             System.IO.StreamWriter SaveFile1 = new System.IO.StreamWriter(sPath);
@@ -52,6 +132,40 @@ namespace lab8
             Runge run = new Runge(t0, T, u0, Convert.ToDouble(comboBox1.SelectedItem.ToString()));
             run.doRunge();
             List<double> runResults = run.uResults;
+
+            Runge run1 = new Runge(t0, T, u0, Convert.ToDouble(comboBox1.Items[1].ToString()));
+            run1.doRunge();
+            List<double> run1Results = run1.uResults;
+
+            Runge run2 = new Runge(t0, T, u0, Convert.ToDouble(comboBox1.SelectedItem.ToString()) / 3);
+            run2.doRunge();
+            List<double> run2Results = run2.uResults;
+            string run1diff = ""; string run2diff = "";
+            o = 0;
+            for (int i = 0; i < runResults.Count; i++)
+            {
+                run1diff += (run1Results[i + o] - runResults[i]).ToString() + "\n";
+                o++;
+            }
+            o = 0;
+            for (int i = 0; i < run1Results.Count; i++)
+            {
+                run2diff += (run2Results[i + o] - run1Results[i]).ToString() + "\n";
+                o += 2;
+            }
+
+            Path = $"difference1Runge.txt";
+            System.IO.StreamWriter SaveFilevb = new System.IO.StreamWriter(Path);
+            SaveFilevb.Write(run1diff);
+            SaveFilevb.ToString();
+            SaveFilevb.Close();
+
+            Path = $"difference2Runge.txt";
+            System.IO.StreamWriter SaveFileav = new System.IO.StreamWriter(Path);
+            SaveFileav.Write(run2diff);
+            SaveFileav.ToString();
+            SaveFileav.Close();
+
             foreach (double res in runResults) listBox3.Items.Add(res);
 
             sPath = $"runge{comboBox1.SelectedItem.ToString()}.txt";
@@ -67,6 +181,41 @@ namespace lab8
             RungeKutt4 rk = new RungeKutt4(t0, T, u0, Convert.ToDouble(comboBox1.SelectedItem.ToString()));
             rk.doRunge();
             List<double> rkResults = rk.uResults;
+
+            RungeKutt4 rk1 = new RungeKutt4(t0, T, u0, Convert.ToDouble(comboBox1.Items[1].ToString()));
+            run1.doRunge();
+            List<double> rk1Results = rk1.uResults;
+
+            RungeKutt4 rk2 = new RungeKutt4(t0, T, u0, Convert.ToDouble(comboBox1.SelectedItem.ToString()) / 3);
+            run2.doRunge();
+            List<double> rk2Results = rk2.uResults;
+            string rk1diff = ""; string rk2diff = "";
+            o = 0;
+            for (int i = 0; i < rkResults.Count; i++)
+            {
+                rk1diff += (rk1Results[i + o] - rkResults[i]).ToString() + "\n";
+                o++;
+            }
+            o = 0;
+            for (int i = 0; i < rk1Results.Count; i++)
+            {
+                rk2diff += (rk2Results[i + o] - rk1Results[i]).ToString() + "\n";
+                o += 2;
+            }
+
+            Path = $"difference1RungeKutt.txt";
+            System.IO.StreamWriter SaveFilevf = new System.IO.StreamWriter(Path);
+            SaveFilevf.Write(rk1diff);
+            SaveFilevf.ToString();
+            SaveFilevf.Close();
+
+            Path = $"difference2RungeKutt.txt";
+            System.IO.StreamWriter SaveFileavawd = new System.IO.StreamWriter(Path);
+            SaveFileavawd.Write(rk2diff);
+            SaveFileavawd.ToString();
+            SaveFileavawd.Close();
+
+
             foreach (double res in rkResults) listBox4.Items.Add(res);
 
             sPath = $"rungeKutt4{comboBox1.SelectedItem.ToString()}.txt";
